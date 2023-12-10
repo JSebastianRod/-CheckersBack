8ffc05145ff34d29ccad5aa28b7924c71c814413
JSebastianRod <juan.rodriguez-pen@mail.escuelaing.edu.co>
2023-12-10 17:48:01 -0500

    server

--- checkers/src/main/java/edu/eci/arsw/checkers/controller/JuegoController.java
+++ checkers/src/main/java/edu/eci/arsw/checkers/controller/JuegoController.java
@@ -13,7 +13,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 /**
  * Controlador que gestiona las operaciones relacionadas con el juego de damas.
  */
-@CrossOrigin(origins = {"http://20.22.63.5", "http://20.22.63.5:8080", "http://checkers.eastus.cloudapp.azure.com" , "http://checkersplay.azurewebsites.net/"})
+@CrossOrigin(origins = {"http://20.22.63.5", "http://20.22.63.5:8080", "http://checkers.eastus.cloudapp.azure.com" , "http://checkersplay.azurewebsites.net/, http://172.178.33.192/ ,http://scheckersplay.eastus.cloudapp.azure.com/"})
 @RestController
 @RequestMapping("/api/juego")
 public class JuegoController {
